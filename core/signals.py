8d6fc0ac2890d33f01b0a443e4255fa0b3e5cccd
Ahmed Khalid <106074266+ahmed-arb@users.noreply.github.com>
2023-01-24 12:23:06 +0500

    feat: update book stock
    When a loan is issued or returned update book stock accordingly

---
+++ core/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.db.models import F
+
+from .models import Book, BookLoan
+
+
+@receiver(pre_save, sender=BookLoan)
+def update_inventory(sender, **kwargs):
+    loan_instance: BookLoan = kwargs["instance"]
+    if loan_instance.id is None:  # new object will be created
+        pass
+    else:
+        loan_previous = BookLoan.objects.get(id=loan_instance.id)
+        if loan_previous.status != loan_instance.status:  # status updated
+            if loan_instance.status == 'issued':
+                Book.objects.filter(pk=loan_instance.book.id).update(stock=F("stock") - 1)
+            elif loan_instance.status == 'returned':
+                Book.objects.filter(pk=loan_instance.book.id).update(stock=F("stock") + 1)
